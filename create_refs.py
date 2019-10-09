40c2846c965016e65829f38d0cda43fd51852bf2
Philip Morse <philipmorse1234@gmail.com>
2019-10-09 19:00:01 -0400

    Script to create ref imgs

--- create_refs.py
+++ create_refs.py
@@ -1,5 +1,4 @@
 import cv2
-import numpy as np
 
 import time
 
@@ -7,9 +6,11 @@ pos1 = [0, 0]
 frame = None
 count = 0
 
+# video file
 video = cv2.VideoCapture("vid2.mp4")
 
 
+# Find Mouse click locations
 def click_event(event, x, y, flags, param):
     global count, pos1
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -18,18 +19,19 @@ def click_event(event, x, y, flags, param):
             pos1[1] = y
             count += 1
         else:
-            get_roi(pos1,[x,y])
-            count+=1
+            get_roi(pos1, [x, y])
+            count += 1
 
 
+# Save img to file
 def get_roi(pos, pos2):
     roi = frame[pos[1]: pos2[1], pos[0]: pos2[0]]
-    filename = 'ref/ball-'+ str(int(time.time())) + '.png'
+    filename = 'ref/ball-' + str(int(time.time())) + '.png'
     print(filename)
-    cv2.imwrite(filename,roi)
+    cv2.imwrite(filename, roi)
 
 
-
+# Main Loop
 while True:
     _, frame = video.read()
 
