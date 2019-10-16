aff652bdced837e5252bd24b82fa5d685f0f91a6
Philip Morse <philipmorse1234@gmail.com>
2019-10-16 17:34:39 -0400

    Added variable kernel size based on video width

--- background_subtraction.py
+++ background_subtraction.py
@@ -3,18 +3,22 @@ from keras.models import load_model
 from keras.preprocessing import image
 import numpy as np
 
-USE_MODEL = True
-MODEL = "0.9818182-1571182058"
-CREATE_TRAINING_DATA = False
-VID_NAME = "vid2"
-VID_FORMAT = ".mp4"
+USE_IMAGE_CLASSIFICATION_MODEL = True  # Determine whether to used image classification model
+MODEL = "0.9818182-1571182058"  # Model name to be used (.h5 format)
+CREATE_TRAINING_DATA = False  # Used to create training set in data/
+VID_NAME = "vid1"  # Video file found in vids/
+VID_FORMAT = ".mp4"  # Video format
+KERNEL_RESOLUTION = 200  # Higher = More objects detected in frame
 
 cap = cv2.VideoCapture("vids/" + VID_NAME + VID_FORMAT)
+cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+kernel_size = int(cap_width / KERNEL_RESOLUTION)
+print(cap_width, kernel_size)
 fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=50)
 
 model = None
 
-if USE_MODEL:
+if USE_IMAGE_CLASSIFICATION_MODEL:
     model = load_model('models/' + MODEL + '.h5')
 
 
@@ -39,7 +43,7 @@ while True:
     ret, frame = cap.read()
     frame_n = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
     fgmask = fgbg.apply(frame)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
     opening = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel, iterations=2)
     opening = cv2.dilate(opening, kernel, iterations=1)
 
@@ -56,7 +60,7 @@ while True:
                                 roi = frame[y: y + h, x: x + w]
                                 filename = 'data/' + VID_NAME + '-' + str(frame_n) + '-' + str(i) + '.png'
                                 cv2.imwrite(filename, roi)
-                if USE_MODEL:
+                if USE_IMAGE_CLASSIFICATION_MODEL:
                     probabilities = classify_contours(contours, frame)
 
                     # TODO: Change this naive ball finding
