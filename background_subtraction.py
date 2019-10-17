37ad7f11a0a96f516657923967833c2206b1a379
Philip Morse <philipmorse1234@gmail.com>
2019-10-17 18:52:26 -0400

    Added working naive location prediction

--- background_subtraction.py
+++ background_subtraction.py
@@ -7,12 +7,12 @@ RUN_VIDEO = True  # Run Main Loop
 USE_IMAGE_CLASSIFICATION_MODEL = True  # Determine whether to used image classification model
 MODEL = "0.9818182-1571182058"  # Model name to be used (.h5 format)
 CREATE_TRAINING_DATA = False  # Used to create training set in data/
-VID_NAME = "vid1"  # Video file found in vids/
+VID_NAME = "vid3"  # Video file found in vids/
 VID_FORMAT = ".mp4"  # Video format
 KERNEL_RESOLUTION = 200  # Higher = More objects detected in frame
 MAX_CONTOURS = 100  # Limit the number of contours for performance
-PREDICT_BALL_LOCATION = False  # Turn on to keep ball location predictions history
-CLASSIFICATION_THRESHOLD = 0  # 0-1 Which contours to evaluate for predictions
+PREDICT_BALL_LOCATION = True  # Turn on to keep ball location predictions history
+#CLASSIFICATION_THRESHOLD = 0  # 0-1 Which contours to evaluate for predictions
 
 cap = cv2.VideoCapture("vids/" + VID_NAME + VID_FORMAT)
 cap_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
@@ -29,7 +29,7 @@ if USE_IMAGE_CLASSIFICATION_MODEL:
 # TODO: Improve prediction code
 class Prediction:
     def __init__(self, frame_n, x, y):
-        self.count = 1
+        self.confidence = 0
         self.hist = [[frame_n, x, y, 0, 0]]  # [frame_n, x, y, dx, dy]
 
     def new_point(self, frame_n, x, y):
@@ -40,7 +40,7 @@ class Prediction:
             (x - self.hist[-1][1]) / (frame_n - self.hist[-1][0]),
             (y - self.hist[-1][2]) / (frame_n - self.hist[-1][0])
         ])
-        self.count += 1
+        self.confidence+=1
 
     def predict(self, frame_n):
         return [
@@ -49,8 +49,8 @@ class Prediction:
         ]
 
 
-    def count(self):
-        return self.count
+    def confidence(self):
+        return self.confidence
 
 
 def classify_contours(contour_list, target_frame):
@@ -66,15 +66,14 @@ def classify_contours(contour_list, target_frame):
         probability = model.predict_proba(img)
         probs.append(probability[0][1])
         cv2.putText(frame, str(probability[0][1]), (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255))
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        #cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     return probs
 
 
 def distance(arr_a, arr_b):
     return ((arr_a[0] - arr_b[0]) ** 2 + (arr_a[1] - arr_b[1]) ** 2) ** 0.5
 
-active_prediction = 1
-predictions = [Prediction(0,cap_width/4,cap_height/2), Prediction(0,cap_width/2,cap_height/2), Prediction(0,cap_width*3/4,cap_height/2)]
+prediction = Prediction(0,cap_width/2,cap_height/2)
 if RUN_VIDEO:
     while True:
         ret, frame = cap.read()
@@ -88,6 +87,7 @@ if RUN_VIDEO:
         if frame_n > 1:
             contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
             if contours:
+                cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
                 if len(contours) < MAX_CONTOURS:
                     if CREATE_TRAINING_DATA:
                         for i, c in enumerate(contours):
@@ -106,18 +106,20 @@ if RUN_VIDEO:
 
                         # TODO: Change this naive ball finding
                         if PREDICT_BALL_LOCATION:
-                            predicted_locations = [predictions[0].predict(frame_n),predictions[1].predict(frame_n),predictions[2].predict(frame_n)]
+                            predicted_location = prediction.predict(frame_n)
                             potentials = []
                             for i, p in enumerate(probabilities):
-                                if p>CLASSIFICATION_THRESHOLD:
-                                    x, y, w, h = cv2.boundingRect(contours[i])
-                                    potentials.append([
-                                        i,
-                                        p,
-                                        distance(predicted_locations[0],[x+w/2,y+h/2]),
-                                        distance(predicted_locations[1],[x+w/2,y+h/2]),
-                                        distance(predicted_locations[2],[x+w/2,y+h/2])
-                                    ])
+                                x, y, w, h = cv2.boundingRect(contours[i])
+                                dist = distance(predicted_location,[x+w/2,y+h/2])
+                                if dist<1:
+                                    dist = 1
+                                potentials.append(p/dist)
+                            max_potential = potentials.index(max(potentials))
+                            x, y, w, h = cv2.boundingRect(contours[max_potential])
+                            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                            prediction.new_point(frame_n,x+w/2,y+h/2)
+
+                            print(potentials)
                                 #TODO: Implement Machine Learning Training
                     else:
                         c = max(contours, key=cv2.contourArea)
@@ -128,7 +130,7 @@ if RUN_VIDEO:
         cv2.imshow('original', frame)
         cv2.imshow('opened', opening)
 
-        k = cv2.waitKey(1)
+        k = cv2.waitKey(0)
         if k == 27:
             break
 
